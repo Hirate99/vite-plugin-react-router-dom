9d3206ac3bedbd68a82ad15bc98878ba5c2570fc
Haonan Su <kevinsu99@outlook.com>
2023-03-17 22:13:03 +0800

    publish @0.0.1 (#1)
    * chore: dist in prettierignore
    
    * build: publish

--- dist/vite-plugin-react-router-dom.js
+++ dist/vite-plugin-react-router-dom.js
@@ -1,82 +1,55 @@
-import { normalizePath as R } from 'vite';
-const $ = (a) => a.replace(/^.*[\\\/]/, '');
+import { normalizePath as R } from "vite";
+const $ = (a) => a.replace(/^.*[\\\/]/, "");
 function E(a, l = {}) {
-  const i = /* @__PURE__ */ new Map(),
-    u = new Array(),
-    m = (r, o = !1) => {
-      (i.get(r) !== void 0 && o) || i.set(r, o);
-    },
-    d = (r, o = '') => {
-      for (const [s, t] of Object.entries(r)) {
-        const n = `${o}${s}`;
-        if (typeof t == 'object')
-          if (t.src) {
-            const e = t,
-              y = e.src,
-              h = e.dynamic ?? !0;
-            m(y, h);
-            const c = {};
-            e.action && (c.action = e.action),
-              e.dynamic && (c.dynamic = e.dynamic),
-              e.errorElement && (c.errorElement = e.errorElement),
-              e.hasErrorBoundary && (c.errorElement = e.hasErrorBoundary),
-              e.loader && (c.loader = e.loader),
-              e.shouldRevalidate && (c.shouldRevalidate = e.shouldRevalidate),
-              u.push({
-                route: n,
-                component: $(y),
-                config: c,
-              });
-          } else d(t, n);
-        else
-          m(t),
-            u.push({
-              route: n,
-              component: $(t),
-            });
-      }
-    },
-    p = () => {
-      const r = [],
-        o = [];
-      for (const [s, t] of i) {
-        const n = $(s);
-        t
-          ? o.push(`const ${n} = lazy(() => import('${s}'))`)
-          : r.push(`import ${n} from '${s}'`);
-      }
-      return [...r, ...o].join(`
+  const i = /* @__PURE__ */ new Map(), u = new Array(), m = (r, o = !1) => {
+    i.get(r) !== void 0 && o || i.set(r, o);
+  }, d = (r, o = "") => {
+    for (const [s, t] of Object.entries(r)) {
+      const n = `${o}${s}`;
+      if (typeof t == "object")
+        if (t.src) {
+          const e = t, y = e.src, h = e.dynamic ?? !0;
+          m(y, h);
+          const c = {};
+          e.action && (c.action = e.action), e.dynamic && (c.dynamic = e.dynamic), e.errorElement && (c.errorElement = e.errorElement), e.hasErrorBoundary && (c.errorElement = e.hasErrorBoundary), e.loader && (c.loader = e.loader), e.shouldRevalidate && (c.shouldRevalidate = e.shouldRevalidate), u.push({
+            route: n,
+            component: $(y),
+            config: c
+          });
+        } else
+          d(t, n);
+      else
+        m(t), u.push({
+          route: n,
+          component: $(t)
+        });
+    }
+  }, p = () => {
+    const r = [], o = [];
+    for (const [s, t] of i) {
+      const n = $(s);
+      t ? o.push(`const ${n} = lazy(() => import('${s}'))`) : r.push(`import ${n} from '${s}'`);
+    }
+    return [...r, ...o].join(`
 `);
-    },
-    f = (r) => {
-      const o = 'async';
-      return Object.entries(r).map(([s, t]) => {
-        if (typeof t == 'function') {
-          let n = t.toString();
-          return (
-            n.startsWith(o) && (n = n.slice(o.length, n.length).trim()),
-            n.startsWith('function')
-              ? `${s}: ${t.toString()}`
-              : `${t.toString()}`
-          );
-        }
-        return `${s}: ${t},`;
-      }).join(`
+  }, f = (r) => {
+    const o = "async";
+    return Object.entries(r).map(([s, t]) => {
+      if (typeof t == "function") {
+        let n = t.toString();
+        return n.startsWith(o) && (n = n.slice(o.length, n.length).trim()), n.startsWith("function") ? `${s}: ${t.toString()}` : `${t.toString()}`;
+      }
+      return `${s}: ${t},`;
+    }).join(`
 `);
-    },
-    g = () =>
-      u.map(
-        ({ route: r, component: o, config: s }) => `
+  }, g = () => u.map(({ route: r, component: o, config: s }) => `
         {
           path: '${r}',
           element: /*#__PURE__*/React.createElement(${o}, null),
           ${f(s ?? {})}
         }
-      `,
-      );
-  return (
-    d(a),
-    `
+      `);
+  return d(a), `
   import React from 'react';
   import { createBrowserRouter } from 'react-router-dom';
   import { lazy } from 'react';
@@ -89,23 +62,22 @@ function E(a, l = {}) {
   );
   
   export default router;
-  `
-  );
+  `;
 }
 function v(a) {
-  const { name: l, option: i, root: u } = a,
-    m = l ?? 'router.config.js',
-    p = `${u ?? __dirname}/${R(m)}`;
+  const { name: l, option: i, root: u } = a, m = l ?? "router.config.js", p = `${u ?? __dirname}/${R(m)}`;
   return {
-    name: 'vite-plugin-react-router-dom',
-    enforce: 'pre',
+    name: "vite-plugin-react-router-dom",
+    enforce: "pre",
     async load(f) {
       if (f.includes(p)) {
         const g = (await import(f)).default;
         return E(g, i);
       }
       return null;
-    },
+    }
   };
 }
-export { v as default };
+export {
+  v as default
+};
--- dist/vite-plugin-react-router-dom.umd.cjs
+++ dist/vite-plugin-react-router-dom.umd.cjs
@@ -1,81 +1,12 @@
-(function (a, s) {
-  typeof exports == 'object' && typeof module < 'u'
-    ? (module.exports = s(require('vite')))
-    : typeof define == 'function' && define.amd
-    ? define(['vite'], s)
-    : ((a = typeof globalThis < 'u' ? globalThis : a || self),
-      (a['vite-plugin-react-router-dom'] = s(a.vite)));
-})(this, function (a) {
-  'use strict';
-  const s = (f) => f.replace(/^.*[\\\/]/, '');
-  function R(f, p = {}) {
-    const u = new Map(),
-      m = new Array(),
-      l = (r, o = !1) => {
-        (u.get(r) !== void 0 && o) || u.set(r, o);
-      },
-      y = (r, o = '') => {
-        for (const [i, e] of Object.entries(r)) {
-          const n = `${o}${i}`;
-          if (typeof e == 'object')
-            if (e.src) {
-              const t = e,
-                h = t.src,
-                E = t.dynamic ?? !0;
-              l(h, E);
-              const c = {};
-              t.action && (c.action = t.action),
-                t.dynamic && (c.dynamic = t.dynamic),
-                t.errorElement && (c.errorElement = t.errorElement),
-                t.hasErrorBoundary && (c.errorElement = t.hasErrorBoundary),
-                t.loader && (c.loader = t.loader),
-                t.shouldRevalidate && (c.shouldRevalidate = t.shouldRevalidate),
-                m.push({ route: n, component: s(h), config: c });
-            } else y(e, n);
-          else l(e), m.push({ route: n, component: s(e) });
-        }
-      },
-      g = () => {
-        const r = [],
-          o = [];
-        for (const [i, e] of u) {
-          const n = s(i);
-          e
-            ? o.push(`const ${n} = lazy(() => import('${i}'))`)
-            : r.push(`import ${n} from '${i}'`);
-        }
-        return [...r, ...o].join(`
-`);
-      },
-      d = (r) => {
-        const o = 'async';
-        return Object.entries(r).map(([i, e]) => {
-          if (typeof e == 'function') {
-            let n = e.toString();
-            return (
-              n.startsWith(o) && (n = n.slice(o.length, n.length).trim()),
-              n.startsWith('function')
-                ? `${i}: ${e.toString()}`
-                : `${e.toString()}`
-            );
-          }
-          return `${i}: ${e},`;
-        }).join(`
-`);
-      },
-      $ = () =>
-        m.map(
-          ({ route: r, component: o, config: i }) => `
+(function(a,s){typeof exports=="object"&&typeof module<"u"?module.exports=s(require("vite")):typeof define=="function"&&define.amd?define(["vite"],s):(a=typeof globalThis<"u"?globalThis:a||self,a["vite-plugin-react-router-dom"]=s(a.vite))})(this,function(a){"use strict";const s=f=>f.replace(/^.*[\\\/]/,"");function R(f,p={}){const u=new Map,m=new Array,l=(r,o=!1)=>{u.get(r)!==void 0&&o||u.set(r,o)},y=(r,o="")=>{for(const[i,e]of Object.entries(r)){const n=`${o}${i}`;if(typeof e=="object")if(e.src){const t=e,h=t.src,E=t.dynamic??!0;l(h,E);const c={};t.action&&(c.action=t.action),t.dynamic&&(c.dynamic=t.dynamic),t.errorElement&&(c.errorElement=t.errorElement),t.hasErrorBoundary&&(c.errorElement=t.hasErrorBoundary),t.loader&&(c.loader=t.loader),t.shouldRevalidate&&(c.shouldRevalidate=t.shouldRevalidate),m.push({route:n,component:s(h),config:c})}else y(e,n);else l(e),m.push({route:n,component:s(e)})}},g=()=>{const r=[],o=[];for(const[i,e]of u){const n=s(i);e?o.push(`const ${n} = lazy(() => import('${i}'))`):r.push(`import ${n} from '${i}'`)}return[...r,...o].join(`
+`)},d=r=>{const o="async";return Object.entries(r).map(([i,e])=>{if(typeof e=="function"){let n=e.toString();return n.startsWith(o)&&(n=n.slice(o.length,n.length).trim()),n.startsWith("function")?`${i}: ${e.toString()}`:`${e.toString()}`}return`${i}: ${e},`}).join(`
+`)},$=()=>m.map(({route:r,component:o,config:i})=>`
         {
           path: '${r}',
           element: /*#__PURE__*/React.createElement(${o}, null),
-          ${d(i ?? {})}
+          ${d(i??{})}
         }
-      `,
-        );
-    return (
-      y(f),
-      `
+      `);return y(f),`
   import React from 'react';
   import { createBrowserRouter } from 'react-router-dom';
   import { lazy } from 'react';
@@ -88,24 +19,4 @@
   );
   
   export default router;
-  `
-    );
-  }
-  function v(f) {
-    const { name: p, option: u, root: m } = f,
-      l = p ?? 'router.config.js',
-      g = `${m ?? __dirname}/${a.normalizePath(l)}`;
-    return {
-      name: 'vite-plugin-react-router-dom',
-      enforce: 'pre',
-      async load(d) {
-        if (d.includes(g)) {
-          const $ = (await import(d)).default;
-          return R($, u);
-        }
-        return null;
-      },
-    };
-  }
-  return v;
-});
+  `}function v(f){const{name:p,option:u,root:m}=f,l=p??"router.config.js",g=`${m??__dirname}/${a.normalizePath(l)}`;return{name:"vite-plugin-react-router-dom",enforce:"pre",async load(d){if(d.includes(g)){const $=(await import(d)).default;return R($,u)}return null}}}return v});
