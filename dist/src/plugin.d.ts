a87b783bcb58a98e5ec066b9fa403bde03eff9af
Haonan Su <kevinsu99@outlook.com>
2023-03-22 16:41:21 +0800

    release 0.0.7

---
+++ dist/src/plugin.d.ts
@@ -0,0 +1,5 @@
+import { PluginOption } from 'vite';
+import type { VitePluginReactRouterDomOptions } from './types';
+declare function vitePluginReactRouterDom(options: VitePluginReactRouterDomOptions): PluginOption;
+export default vitePluginReactRouterDom;
+//# sourceMappingURL=plugin.d.ts.map
---
+++ dist/src/transform.d.ts
@@ -0,0 +1,4 @@
+import type { Route, RouterOption } from './types';
+declare function transform(routes: Route, options?: RouterOption): string;
+export default transform;
+//# sourceMappingURL=transform.d.ts.map
