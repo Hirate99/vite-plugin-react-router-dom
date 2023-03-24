e2922c454b146ed51df62c8feac03d657e4f5bda
Haonan Su <kevinsu99@outlook.com>
2023-03-24 11:27:38 +0800

    feat: support children in react-router

--- example/router.config.js
+++ example/router.config.js
@@ -10,7 +10,19 @@ export default {
     },
   },
   '/dev': {
-    '/': './src/App',
+    '/': {
+      src: './src/App',
+      children: {
+        '/dev/children': {
+          src: './src/Demo',
+          dynamic: true,
+          async loader() {
+            console.log('hello, children');
+            return null;
+          },
+        },
+      },
+    },
     '/demo': './src/App',
   },
 };
